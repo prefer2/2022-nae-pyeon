d059d7b44b27dee2887edb8f3b6fcf28a080f6cb
prefer2 <sunho6200@gmail.com>
2022-08-18 02:23:01 +0900

    time

--- frontend/cypress/e2e/MainPage.cy.js
+++ frontend/cypress/e2e/MainPage.cy.js
@@ -38,26 +38,24 @@ describe("로그인한 사용자 동작", () => {
   // });
 
   it("방문하기", () => {
-    cy.intercept("GET", "https://neapyeon.site/api/v1/members/me", {
+    cy.intercept("GET", "/api/v1/members/me", {
       body: {
         id: 123,
         username: "승팡",
         email: "email@email.com",
       },
-    }).as("getMe");
-
-    cy.wait("@getMe").then(() => {
-      cy.intercept("GET", "/api/v1/teams/me?*", {
-        statusCode: 200,
-        body: {
-          totalCount: 0,
-          currentPage: 0,
-          teams: [],
-        },
-      }).as("getMyTeams");
     });
 
-    cy.wait("@getMyTeams").then(() => {
+    cy.intercept("GET", "/api/v1/teams/me?*", {
+      statusCode: 200,
+      body: {
+        totalCount: 0,
+        currentPage: 0,
+        teams: [],
+      },
+    }).as("getMyTeams");
+
+    cy.wait("3000").then(() => {
       cy.contains("아직 참여한 모임이 없어요!").should("be.visible");
     });
   });
